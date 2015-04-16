4663c7e7afd6055db5a945191841f15ff7195c5a
Summer <throwawaysummer@gmail.com>
2015-04-16 12:45:27 -0500

    NO MORE collectionView
    Changed collectionView to TableView, may make another branch to deal
    with these changes

--- PressBadge/BadgeViewController.swift
+++ PressBadge/BadgeViewController.swift
@@ -8,12 +8,37 @@
 
 import UIKit
 
-class BadgeViewController: UIViewController {
-
+class BadgeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
-
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        
+    }
+    
+    
+    @IBOutlet weak var badgeStatusTable: UITableView!
+    
+    let swiftBlogs = ["Ray Wenderlich", "NSHipster", "iOS Developer Tips", "Jameson Quave", "Natasha The Robot", "Coding Explorer", "That Thing In Swift", "Andrew Bancroft", "iAchieved.it", "Airspeed Velocity"]
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1 // This was put in mainly for my own unit testing
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return swiftBlogs.count // Most of the time my data source is an array of something...  will replace with the actual name of the data source
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = badgeStatusTable.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        // set cell's textLabel.text property
+        // set cell's detailTextLabel.text property
+        return cell
+    }
+    
 }
--- PressBadge/StatusViewController.swift
+++ PressBadge/StatusViewController.swift
@@ -19,8 +19,26 @@ class StatusViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
     }
     
+    
     @IBOutlet weak var badgeStatusTable: UITableView!
     
+    let swiftBlogs = ["Ray Wenderlich", "NSHipster", "iOS Developer Tips", "Jameson Quave", "Natasha The Robot", "Coding Explorer", "That Thing In Swift", "Andrew Bancroft", "iAchieved.it", "Airspeed Velocity"]
+
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1 // This was put in mainly for my own unit testing
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return swiftBlogs.count // Most of the time my data source is an array of something...  will replace with the actual name of the data source
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = badgeStatusTable.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        // set cell's textLabel.text property
+        // set cell's detailTextLabel.text property
+        return cell
+    }
 
 }
