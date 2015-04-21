a40b887f631bfd11f80c2282834edb0b44b406d1
Thenry945 <travis@henrichs.cc>
2015-04-21 12:50:00 -0500

    Merge remote-tracking branch 'origin/UIBadgeViewLogic' into ParsingAlgorithm
    Conflicts:
    	PressBadge.xcodeproj/project.pbxproj

--- PressBadge/BadgeViewController.swift
+++ PressBadge/BadgeViewController.swift
@@ -8,12 +8,43 @@
 
 import UIKit
 
-class BadgeViewController: UIViewController {
-
+class BadgeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        image.image = UIImage(named: "Life1badge.png")
 
     }
-
-
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        
+    }
+    
+    @IBOutlet weak var badgeStatusTable: UITableView!
+    
+    let badges = ["Life1badge.png", "Life2badge.png", "Life3badge.png"]
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return badges.count // Most of the time my data source is an array of something...  will replace with the actual name of the data source
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "cell"
+        
+        var cell : UITableViewCell
+        
+        cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as! UITableViewCell
+        cell.textLabel!.text = self.badges[indexPath.row]
+        var image : UIImage = UIImage(named: "Life2badge.png")!
+        return cell
+    }
+    
 }
--- PressBadge/StatusViewController.swift
+++ PressBadge/StatusViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class StatusViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var image: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,6 +25,20 @@ class StatusViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     @IBOutlet weak var badgeStatusTable: UITableView!
     
+    let badges = ["Life1badge.png", "Life2badge.png", "Life3badge.png"];
+
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return badges.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = badgeStatusTable.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
+        return cell
+    }
 
 }
